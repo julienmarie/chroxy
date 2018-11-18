50fe56e6c6b682bf5c2c7c7cb88dc9d587e1e668
Julien Marie <hi@producture.com>
2018-11-18 11:53:30 +0800

    Update application.ex

--- lib/chroxy/application.ex
+++ lib/chroxy/application.ex
@@ -9,6 +9,7 @@ defmodule Chroxy.Application do
   def start(_type, _args) do
     # HACK to get exec running as root.
     Application.put_env(:exec, :root, true)
+    Exexec.start(%{port_path: "/usr/local/bin/exec-port"})
     {:ok, _} = Application.ensure_all_started(:erlexec)
 
     proxy_opts = Application.get_env(:chroxy, Chroxy.ProxyListener)
